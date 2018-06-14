6da92bb96899c2525fbf0997c44ca4463741ca8d
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-06-14 18:46:16 +0200

    Proper name and tags for the docker images

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -ex
 
 for PLATFORM in "i686" "x86_64"; do
-    docker build --rm -t manylinux1_$PLATFORM -f Dockerfile-$PLATFORM .
+    docker build --rm -t mtgupf/essentia-builds:manylinux1_$PLATFORM -f Dockerfile-$PLATFORM .
 done
