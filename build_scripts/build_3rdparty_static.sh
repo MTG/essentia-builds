40a2c70e42a3791f922cc480dd38c00980145625
pabloEntropia <pablo.alonso@upf.edu>
2018-12-17 19:53:14 +0100

    add custom packaing script with tensorflow support

--- build_scripts/build_3rdparty_static.sh
+++ build_scripts/build_3rdparty_static.sh
@@ -1,12 +1,16 @@
 set -e -x
 
 # Check out a particular commit for building dependencies
-curl -SLO https://github.com/MTG/essentia/archive/$ESSENTIA_3RDPARTY_VERSION.zip
+curl -SLO https://github.com/pabloEntropia/essentia/archive/$ESSENTIA_3RDPARTY_VERSION.zip
 unzip $ESSENTIA_3RDPARTY_VERSION.zip
-cd essentia-*/
+
+# The folder name has to be trimmed or the absolute path of the objects would
+# generate "input line too long" errors.
+mv essentia-*/ essentia
+cd essentia
 
 # Install dependencies to /usr/local; force --static flag to pickup private libraries for Qt
 PKGCONFIG="/usr/bin/pkg-config --static" ./packaging/build_3rdparty_static_debian.sh --with-gaia
 
 cd ..
-rm -r essentia-* $ESSENTIA_3RDPARTY_VERSION.zip
+rm -r essentia $ESSENTIA_3RDPARTY_VERSION.zip
