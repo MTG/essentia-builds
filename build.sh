9bba88aaec5bcec637adb05a12a91015be0aa855
pabloEntropia <pablo.alonso@upf.edu>
2018-12-17 19:47:19 +0100

    change manylinux1 to manylinux2

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -ex
 
 for PLATFORM in "i686" "x86_64"; do
-    docker build --rm -t mtgupf/essentia-builds:manylinux1_$PLATFORM -f Dockerfile-$PLATFORM .
+    docker build --rm -t mtgupf/essentia-builds:manylinux2_$PLATFORM -f Dockerfile-$PLATFORM .
 done
