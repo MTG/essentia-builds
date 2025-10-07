977156961da3769c2b49ace9c3f6b30a719afced
xaviliz <xavilizarraga@gmail.com>
2025-10-07 14:38:23 +0200

    Updated script to build essentia dependencies

--- build_scripts/build_3rdparty_static_debian.sh
+++ build_scripts/build_3rdparty_static_debian.sh
@@ -1,24 +1,29 @@
-set -e -x
+#!/usr/bin/env bash
+set -e
+BASEDIR=$(dirname $0)
+cd $BASEDIR/debian_3rdparty
+./build_eigen3.sh
+./build_fftw3.sh
+./build_lame.sh
+./build_ffmpeg.sh
+./build_libsamplerate.sh
+./build_zlib.sh
+./build_taglib.sh
+./build_yaml.sh
+./build_chromaprint.sh
+./build_onnx.sh
 
-# Check out a particular commit for building dependencies
-curl -SLO https://github.com/MTG/essentia/archive/$ESSENTIA_3RDPARTY_VERSION.zip
-unzip $ESSENTIA_3RDPARTY_VERSION.zip
-
-cp /build_zlib.sh essentia-*/packaging/debian_3rdparty/
-chmod +x /build_onnx.sh
-cp /build_onnx.sh essentia-*/packaging/debian_3rdparty/
-cp /build_3rdparty_static_debian.sh /build_config.sh essentia-*/packaging/
-cp /build_chromaprint.sh essentia-*/packaging/debian_3rdparty/
-cp /build_taglib.sh essentia-*/packaging/debian_3rdparty/
-
-cd essentia-*/
-
-if [[ ${WITH_TENSORFLOW} ]] ; then
-    with_tensorflow=--with-tensorflow
+#!/usr/bin/env bash
+if [[ "$*" == *--with-gaia* ]]
+    then
+        ./build_qt.sh
+        ./build_gaia.sh
+        rm -rf mkspecs plugins translations
 fi
 
-# Install dependencies to /usr/local; force --static flag to pickup private libraries for Qt
-PKGCONFIG="/usr/bin/pkg-config --static" ./packaging/build_3rdparty_static_debian.sh
+if [[ "$*" == *--with-tensorflow* ]]
+    then
+        ./build_tensorflow.sh
+fi
 
-cd ..
-rm -r essentia-* $ESSENTIA_3RDPARTY_VERSION.zip
+rm -rf bin share
