ae82f0fd685079ff49f59f3293545699e65d8953
xaviliz <xavilizarraga@gmail.com>
2026-03-09 15:30:28 +0100

    Update scripts to install and unistall CUDA12.3 and CUDNN9

--- build_scripts/uninstall_cuda.sh
+++ build_scripts/uninstall_cuda.sh
@@ -1,9 +1,17 @@
 set -e -x
 
 # Uninstall CUDA and cuDNN so after TensorFlow is built.
-rpm -e libcudnn8-devel
-rpm -e libcudnn8
-yum autoremove -y cuda
+yum remove -y 'libcudnn9-devel-cuda-12'
+yum remove -y \
+  cuda-nvcc-* \
+  cuda-cudart-devel-* \
+  cuda-libraries-devel-* \
+  cuda-toolkit-*
+
+# Autoclean unused deps
+yum autoremove -y
+yum clean all
+rm -rf /var/cache/yum /var/Cache/dnf
 
 # Remove missing cuDNN libs
 rm -rf /usr/local/cuda-*
